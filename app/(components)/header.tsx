3ab81caf64e77231616ef7ec196fc51452d5e9ba
Joshua Rashtian <joshrashtian1@gmail.com>
2026-01-25 21:57:03 -0800

    add header component

---
+++ app/(components)/header.tsx
@@ -0,0 +1,44 @@
+import { Gasoek_One } from "next/font/google";
+import Link from "next/link";
+import React from "react";
+const fontFamily = Gasoek_One({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
+
+const navigation = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Papers",
+    href: "/papers",
+  },
+  {
+    label: "Create",
+    href: "/create",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="dark:bg-zinc-900 dark:text-white bg-white text-black w-full flex justify-between items-center px-12 py-4">
+      <h1 className={`${fontFamily.className} text-2xl font-bold`}>
+        mindfieldia
+      </h1>
+      <nav>
+        <ul className="flex items-center gap-4">
+          {navigation.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import Header from "./(components)/header";
+const dmSans = DM_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -24,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.className} antialiased`}>
+        {" "}
+        <Header />
         {children}
       </body>
     </html>
